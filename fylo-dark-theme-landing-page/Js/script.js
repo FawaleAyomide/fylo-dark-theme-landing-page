66091b14f9ae37471abcae280e3709b48f99746a
Fawale Ayomide Oladele <109774878+FawaleAyomide@users.noreply.github.com>
2023-07-20 17:24:40 +0100

    Delete fylo-dark-theme-landing-page directory

--- fylo-dark-theme-landing-page/Js/script.js
+++
@@ -1,7 +0,0 @@
-const toggler = document.querySelector('.navBarToggler');
-const sideBar = document.querySelector('.top');
-
-toggler.onclick = function () {
-    toggler.classList.toggle('openMenu');
-    sideBar.classList.toggle('animate');
-};
